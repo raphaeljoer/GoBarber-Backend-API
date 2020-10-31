c5bbab450de9bebe293ffe7d5516129dd3611324
raphaeljoer <ra.joer@gmail.com>
2020-10-31 19:33:55 -0300

    add data validation on routes

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import { errors } from 'celebrate';
 import 'express-async-errors';
 import 'reflect-metadata';
 
@@ -16,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+app.use(errors());
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
